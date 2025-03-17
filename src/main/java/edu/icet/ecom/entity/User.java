5db74dd2e55a930a69ff0620dfef7f12f996f4ea
DESHIKATHIKSHANI <deshikathikshani456@gmail.com>
2025-03-18 00:23:55 +0530

    User Entity and Dto

---
+++ src/main/java/edu/icet/ecom/entity/User.java
@@ -0,0 +1,34 @@
+package edu.icet.ecom.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public User() {
+    }
+
+    public User(String username, String password, Role role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public enum Role {
+        ADMIN,
+        CASHIER
+    }
+}
